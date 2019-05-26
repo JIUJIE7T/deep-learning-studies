d636c2e682f68c869fa96feac2b73051f9ec1a69
Jie Zhou <jiujie.t@hotmail.com>
2019-05-26 23:38:59 +0900

    Chapter 2 Perceptron Practice

---
+++ perceptron-practice-xor.py
@@ -0,0 +1,29 @@
+def AND(x1,x2):
+  w1, w2, theta = 0.5,0.5, 0.7
+  tmp = x1*w1 + x2*w2
+  if tmp <= theta:
+    return 0
+  elif tmp > theta:
+    return 1
+
+def OR(x1,x2):
+  w1,w2, theta = 1,1,0.2
+  tmp = x1*w1 + x2*w2
+  if tmp <= theta:
+    return 0
+  elif tmp > theta:
+    return 1
+
+def NAND(x1,x2):
+  w1,w2, theta = -0.5,-0.5,-0.7
+  tmp = x1*w1 + x2*w2
+  if tmp <= theta:
+    return 0
+  elif tmp > theta:
+    return 1
+
+def XOR(x1,x2):
+  s1 = NAND(x1,x2)
+  s2 = OR(x1,x2)
+  r = AND(s1,s2)
+  return r
